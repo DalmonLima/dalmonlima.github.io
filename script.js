0ae650f7cd68ba43f7161b82b7b4716c1c0a754a
Dalmon Lima <ozeiasdsl@gmail.com>
2019-06-06 02:27:48 -0300

    fixing bugs

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 
 function toggleMenuControl(){
-  $('#menu-toggler a').click(function() {
-    $('.menu-mobile').addClass('open');
-    $('#cover').css("display", "block");
+  $( '#menu-toggler a' ).click(function() {
+    $( '.menu-mobile' ).addClass('open');
+    $( '#cover' ).css("display", "block");
   });
 
-  $('#close-menu').click(function() {
-    $('.menu-mobile').removeClass('open');
-    $('#cover').css("display", "none");
+  $( '#close-menu' ).click(function() {
+    $( '.menu-mobile' ).removeClass('open');
+    $( '#cover' ).css("display", "none");
   });
 }
 
+// function seePortfolio(){
+//   $( '.portfolio-card' ).mouseover(function(){
+//     var selectedProject = $(this);
+//     selectedProject.find( '.card-info' ).css( "display", "block" );
+//     selectedProject.mouseleave(function(){
+//       selectedProject.find( '.card-info' ).css( "display","none" )
+//     });
+//   });
+// }
+
 
 toggleMenuControl();
+// seePortfolio();
